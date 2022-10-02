c513f9c5e18bb72f12d8da2dfad87a4746a90b22
Patrick Sweeney <psweeney101@gmail.com>
2022-10-02 02:19:53 -0400

    Add wildcard route

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
 import { GMChatType } from 'src/interfaces';
 import { About, Callback, Chat, Home, Login, Logout } from 'src/pages';
@@ -23,6 +23,7 @@ root.render(
         <Route path="/callback" element={<Callback />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </RouterScroll>
   </BrowserRouter>
