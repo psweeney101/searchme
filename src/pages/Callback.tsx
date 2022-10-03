3102dc8cede126d2184137dae11701343ed4785a
Patrick Sweeney <psweeney101@gmail.com>
2022-10-02 23:35:58 -0400

    Fix redirect_url

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -9,13 +9,15 @@ export function Callback(props: Props): ReactElement {
 
   if (access_token) {
     GroupMe.access_token = access_token;
-    GroupMe.getUser().catch(() => window.location.href = '/logout');
-
-    const redirect_url = sessionStorage.getItem('redirect_url');
-    if (redirect_url) {
-      sessionStorage.removeItem('redirect_url');
-      window.location.href = redirect_url;
-    }
+    GroupMe.getUser().then(() => {
+      const redirect_url = sessionStorage.getItem('redirect_url');
+      if (redirect_url) {
+        sessionStorage.removeItem('redirect_url');
+        window.location.href = redirect_url;
+      }
+    }).catch(() => {
+      window.location.href = '/logout'
+    });
   }
 
   return <Navigate to="/" />
