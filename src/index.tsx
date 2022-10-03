32d2374828d76a35e68dbd632001ef62a36e6aaf
Patrick Sweeney <psweeney101@gmail.com>
2022-10-02 22:38:43 -0400

    Redirect to heroku from any page

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import { About, Callback, Chat, Home, Login, Logout } from 'src/pages';
 import { RouterScroll } from 'src/components';
 import { App } from './App';
 
+// Automatically redirect from Heroku to Vercel
+if (window.location.hostname.includes('herokuapp.com')) {
+  window.alert('SearchMe is moving to https://searchme.vercel.app. Heroku is ending its free tier support on November 28, 2022. Starting then, this herokuapp.com URL will no longer be available. Redirecting now...');
+  window.location.href = 'https://searchme.vercel.app';
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
